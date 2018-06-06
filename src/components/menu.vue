63b3ad616f981736423f76fb7453d14b33846c6f
Luis Gonzalez <luisal1207@hotmail.es>
2018-06-05 22:23:16 -0600

    Cambia al menu mas sencillo.

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,33 +1,10 @@
 <template>
   <div class="Menu">
-    <div class="ControlLineTime">
-      <Scroll
-        v-for="(Item , key) in BartScroll"
-        :key="key"
-        :index="key"
-        :ictive="Item.Active"/>
-    </div>
-    <img
-      class="EngraneA"
-      src="@/assets/Img/EngraneFinal.svg">
-    <div
-      class="Profundidad"/>
-    <div
-      class="ModulePrev">
-      <div
-        class="IconModule"><img src="@/assets/Img/IconIncognito.svg"></div>
-      <div
-        class="IconLlave"><img src="@/assets/Img/LLaveIcon.svg"></div>
-    </div>
-    <div class="MenuTitle">
-      <a
-        href="#"
-        class="SigBt"><h1 class="icon-left-open"/></a>
-      <h1
-        class="TitleModule">¿Que es Inteligencia?</h1>
-      <a
-        href="#"
-        class="SigBt"><h1 class="icon-right-open"/></a>
+    <div class="menu-container">
+      <img
+        class="menu-svg"
+        src="@/assets/Img/Menu.svg"
+        alt="Menu">
     </div>
   </div>
 </template>
@@ -52,79 +29,26 @@ export default {
   }
 };
 </script>
-<style lang="sass" scoped>
-.Menu
-  position: relative
-  width: 100vw
-  height: 100vh
-  overflow: hidden
-  background: white
+<style lang="scss" scoped>
+.Menu{
+  position: relative;
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
+  background: white;
 
-  .ControlLineTime 
-    position: absolute
-    margin: 0
-    margin-top: 65vh
-    margin-left: 2%
-    height: 5h
+  .menu-svg{
+    max-height: 100vh;
+    max-width: 100vw;
+  }
 
-  .EngraneA 
-    position: absolute
-    width: 200px
-    left: -100px
-    bottom: -100px
-    z-index: 15
+  @media (min-width: 640px) {
+    .Menu .menu-svg{
+      max-height: 800vh;
+      max-width: 80vw;
+    }
+  }
+}
 
-  .Profundidad 
-    position: absolute
-    width: 100vw
-    height: 100vh
-    background: radial-gradient(rgba(255, 255, 255, 0) 20%, #353535 160%)
-    z-index: 12
-
-  .ModulePrev 
-    position: relative
-    width: 30%
-    height: 20vh
-    margin: 0
-    margin-left: 32.5%
-    margin-top: 15vh
-
-  .IconModule 
-    width: 30%
-    margin: 0 35%
-    opacity: 0.9
-
-  .IconLlave 
-    margin-top: -5%
-
-  .MenuTitle 
-    position: absolute
-    z-index: 20
-    width: 30%
-    margin: 0 35%
-    height: 10vh
-    margin-top: 20%
-    /* color:#353535*/
-
-  .TitleModule 
-    width: 60%
-    /*  font-size: 2.5vw
-      text-align: center
-      float: left
-      
-      font-weight: bold*/
-
-  .SigBt 
-    width: 20%
-    /*text-decoration: none
-      font-size: 2.5vw
-      float: left
-    
-      text-align: center
-      margin: 5% 0
-      color:#353535*/
-
-  .SigBt > h1:hover 
-    /* color: white*/
 
 </style>
